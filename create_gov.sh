a24d4615d674b811e720a405df54470ff33e89da
oguzemre <oguzemre926@gmail.com>
2022-07-16 23:25:27 +0200

    coverage re-inserted

--- create_gov.sh
+++ create_gov.sh
@@ -5,14 +5,19 @@ $PWD/unittest/build/bin/unittest
 echo 'Gcov process started...'
 mkdir gcov-coverage
 cd gcov-coverage
+cp $PWD/../unittest/build/CMakeFiles/unittest.dir .
 
 for x in $PWD/../unittest/build/CMakeFiles/unittest.dir/*.cpp.o; do
   echo "x: $x"
   gcov "$x"
 done
 
+cp -r $PWD/../unittest/build/CMakeFiles/unittest.dir/ .
+
 
 #Create HTML files of the coverage data:
 lcov --directory . --capture --output-file app.info
+cp app.info ../
+cd ../
 
-genhtml app.info
+genhtml ../app.info
